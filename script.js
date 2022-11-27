2542123b3afb9c57d2946158f78782aa53a6e105
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2022-11-27 15:24:33 +0800

    Added site functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+let myContent = document.querySelector(".result");
+let input = document.querySelector("input");
+let button = document.querySelector("button")
+let i = 0;
+let array = [];
+
+let n = input.value;
+
+function myFunction() {
+    let x = 1 - n;
+    if (x == n * 2) x = n * 2;
+    else x = x - n * i * 0.1;
+    i += 1;
+
+    array.push(x)
+    let result = (1 - array.reduce((a, b) => a * b, 1))
+    myContent.textContent = 'Attempt #' + i + ': ' + (result * 100).toFixed(2) + '%';
+}
+
+input.addEventListener('input', () => {
+    n = input.value / 100;
+    array = []
+    i = 0;
+});
+
+function setStyle(translate, color) {
+    button.style.transform = translate;
+    button.style.backgroundColor = color;
+}
+
+function removeStyle(i) {
+    setTimeout(function () {
+        setStyle('translateY(-2px)', '#ffffff');
+        if (--i) removeStyle(i);
+    }, 110)
+};
+
+input.addEventListener("keyup", ({ key }) => {
+    if (key === "Enter") {
+        myFunction();
+        setStyle('translateY(2px)', '#3e7b8e');
+        removeStyle(1);
+    }
+})
+
+button.onclick = () => {
+    myFunction();
+    setStyle('translateY(2px)', '#3e7b8e');
+    removeStyle(1);
+};
+
+
